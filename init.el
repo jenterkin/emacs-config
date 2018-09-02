51a2c27fe73e2aeb2a44a801abce92563a0806df
Jordan Enterkin <jordan.a.enterkin@gmail.com>
2018-09-02 18:22:49 -0400

    add auto-generated init file

--- init.el
+++ init.el
@@ -1,1 +1,15 @@
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode helm-ag helm-projectile helm which-key use-package spacemacs-theme spaceline-all-the-icons solaire-mode projectile ox-twbs org-bullets neotree md4rd linum-relative htmlize go-mode evil-org evil-magit evil-leader dracula-theme doom-themes dockerfile-mode docker diminish base16-theme anaconda-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
